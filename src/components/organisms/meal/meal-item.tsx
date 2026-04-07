6f9d97ee974847d418a3195690a2530bdc6be556
Diaz-Adrianz <diazz.developer@gmail.com>
2026-04-07 16:47:40 +0700

    style(meal): meal item color

--- src/components/organisms/meal/meal-item.tsx
+++ src/components/organisms/meal/meal-item.tsx
@@ -24,7 +24,7 @@ const MealItem = ({
           height={500}
           className="h-auto w-full object-cover transition-transform group-hover:scale-110"
         />
-        <div className="from-background dark text-foreground group-hover:from-primary pointer-events-none absolute inset-0 top-auto bottom-0 left-0 flex h-40 flex-col justify-end bg-linear-to-t p-3 transition-colors">
+        <div className="dark from-background text-foreground group-hover:text-primary-foreground group-hover:from-primary pointer-events-none absolute inset-0 top-auto bottom-0 left-0 flex h-40 flex-col justify-end bg-linear-to-t p-3 transition-colors">
           <h4 className="typo-heading-sm line-clamp-2">{item.strMeal}</h4>
         </div>
       </Link>
