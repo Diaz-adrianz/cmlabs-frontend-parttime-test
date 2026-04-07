4c5fc2aff3f6f3ec99205e9463ae695d6d17d7f1
Diaz-Adrianz <diazz.developer@gmail.com>
2026-04-07 16:08:04 +0700

    style(ingredient): slugify name for route

---
+++ src/utils/string.ts
@@ -0,0 +1,18 @@
+export const toSlug = (str: string, separator = '-'): string => {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, separator)
+    .replace(/^-+|-+$/g, '');
+};
+
+export const getRandomString = (length: number) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
